83fca56051433c0c9c935ad54ede0251bcbbcb1e
KIIICH <KIIICH>
2021-08-25 20:51:47 +0300

    Add main UI

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Николай on 25.08.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
